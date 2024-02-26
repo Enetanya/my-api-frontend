d91ab35c3ac83a0183c1977c8907adffef184481
Okechukwu Enetanya <129814429+Enetanya@users.noreply.github.com>
2024-02-26 14:15:30 +0000

    Update AutoRedirect.js

--- my-api-frontend production/src/AutoRedirect.js
+++ my-api-frontend production/src/AutoRedirect.js
@@ -1,10 +1,10 @@
 
-import  { useEffect } from 'react';
+
 
 const AutoRedirect = () => {
-  useEffect(() => {
+  
     window.location.href = '/new-login-details';
-  }, []);
+ 
 
   return null; // Since this component is handling redirection, it doesn't render anything
 };
