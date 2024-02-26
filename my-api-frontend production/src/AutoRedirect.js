5ac5e15706b323aa58101a7f9e0da7509f3aac56
Okechukwu Enetanya <129814429+Enetanya@users.noreply.github.com>
2024-02-26 13:52:53 +0000

    Update AutoRedirect.js

--- my-api-frontend production/src/AutoRedirect.js
+++ my-api-frontend production/src/AutoRedirect.js
@@ -1,9 +1,12 @@
 
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
 
 const AutoRedirect = () => {
-  const navigate = useNavigate();
-  navigate('/new-login-details');
-  };
+  useEffect(() => {
+    window.location.href = '/new-login-details';
+  }, []);
+
+  return null; // Since this component is handling redirection, it doesn't render anything
+};
 
 export default AutoRedirect;
